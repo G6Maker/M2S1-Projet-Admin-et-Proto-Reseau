9423c5734f8aea49054fe6854e3da713cdee48cf
Maxime <caanyone@gmail.com>
2024-01-11 15:22:01 +0100

    php, tools update

--- tools.sh
+++ tools.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-read -p "Enter 'service name' to execute port forwarding: " input
+service_name=$1
 
-if [[ $input == "argocd" ]]; then
+if [[ $service_name == "argocd" ]]; then
     echo "mot de passe: $(kubectl get secret argocd-initial-admin-secret -n argocd --template={{.data.password}} | base64 -d)"
     kubectl port-forward service/argocd-server 8443:443 -n argocd
-elif [[ $input == "nginx" ]]; then
+elif [[ $service_name == "nginx" ]]; then
     kubectl port-forward service/nginx 9012:80 -n argocd
 else
-    echo "port-forward du service/$input si possible"
-    kubectl port-forward service/$input 32101:80 -n argocd
+    echo "port-forward du service/$service_name si possible"
+    kubectl port-forward service/$service_name 32101:80 -n argocd
 fi
-    
 
