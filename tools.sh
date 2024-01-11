3fb31c85eb71f2ea4eb434183926d42d977d2e5a
Maxime <caanyone@gmail.com>
2024-01-11 18:46:18 +0100

    nginx service

--- tools.sh
+++ tools.sh
@@ -6,7 +6,7 @@ if [[ $service_name == "argocd" ]]; then
     echo "mot de passe: $(kubectl get secret argocd-initial-admin-secret -n argocd --template={{.data.password}} | base64 -d)"
     kubectl port-forward service/argocd-server 8443:443 -n argocd
 elif [[ $service_name == "nginx" ]]; then
-    kubectl port-forward service/nginx 9012:80 -n argocd
+    kubectl port-forward service/nginx 9012:443 -n argocd
 else
     echo "port-forward du service/$service_name si possible"
     kubectl port-forward service/$service_name 32101:80 -n argocd
